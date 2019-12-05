42e6e8ddfca051aefa466737a41b1ad58a41dbb4
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-05 15:08:12 -0800

    added the handleSubmit function to the App.js component to update the state with the new item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import Todo from './components/Todo';
 import TodoList from './components/TodoList';
-
+import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends Component {
   state = {
     items: [],
-    id: 0,
+    id: uuid(),
     item: '',
     edit: false
   }
@@ -15,14 +15,35 @@ export default class App extends Component {
     this.setState({ item: e.target.value });
   }
 
+  handleSubmit = e => {
+   e.preventDefault();
+
+   const newItem = {
+     id: this.state.id,
+     item: this.state.item
+   }
+
+   const newItems = [...this.state.items,newItem]
+   this.setState({
+     items:newItems,
+     item:'',
+     id: uuid(),
+     edit: false
+   })
+  }
+
+
   render() {
     return (
       <div className='container'>
         <div className='row'>
          <div className='col-10 mx-auto col-md-8 mt-4'>
            <h3 className='text-capitalized text-center'>Add New Items Below</h3>
-          <Todo item={this.state.item} handleChange={this.handleChange} />
-          <TodoList />
+          <Todo item={this.state.item}
+           handleChange={this.handleChange}
+           handleSubmit={this.handleSubmit}
+          />
+          <TodoList items={this.state.items} />
          </div>
         </div>
       </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,17 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
     render() {
+        const { items } = this.props;
         return (
           <ul className='list-group'>
               <h2 className='text-capitalize text-center'>I Want To...</h2>
-              <TodoItem />
+              {
+                  items.map(item => {
+                      return(
+                     <TodoItem key={item.id} title={item.title} />
+                      )
+                  })
+              }
               
               <button type='button' className='btn btn-outline-danger btn-block text-capitalize mt-3'>Delete All</button>
           </ul>
