f0b9f0f31eba7a2ce48c92026b3b7c7346a1d4fb
Argenis De Los Santos <argenis04_09@icloud.com>
2019-12-05 13:31:46 -0800

    added bootstrap to the todo.js component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,9 +4,21 @@ export default class Todo extends Component {
 
     render() {
         return (
-            <div>
-         Hello Todo
-            </div>
+        <div className='card card-body my-3'>
+           <form>
+               <div className='input-group'>
+                <div className='input-group-prepend'>
+                <div className='input-group-text bg-primary text-white'>
+                <i className='fas fa-book'></i>
+                </div>
+                </div>
+
+                <input type='text' className='form-control' placeholder='add item here' />
+
+               </div>
+               <button type='submit' className='btn btn-block btn-outline-primary mt-2'>Add Item</button>
+           </form>
+        </div>
         )
     }
 }
